509e3409e8a2dc5db97e02953b0b9cc6e4793612
oiu850714 <oiu850714@gmail.com>
2020-06-16 16:38:31 +0800

    modify registry wrapper demo code

--- MyRegistryWrapper/MyRegistryWrapper.cpp
+++ MyRegistryWrapper/MyRegistryWrapper.cpp
@@ -7,12 +7,20 @@
 int main()
 {
     std::wstring testSubkey = L"Name\\Your\\Subkey";
-    std::wstring testValue = L"NameYourValue";
+    std::wstring testDwordValue = L"NameYourValue";
+    std::wstring testQwordValue = L"NameYourValue";
+    std::wstring testStringValue = L"NameYourValue";
 
     try
     {
-        DWORD data = RegGetDword(HKEY_CURRENT_USER, testSubkey, testValue);
-        std::wcout << testSubkey << L"\\" << testValue << L" DWORD data: " << data << std::endl;
+        DWORD dwordData = RegGetDword(HKEY_CURRENT_USER, testSubkey, testDwordValue);
+        std::wcout << testSubkey << L"\\" << testDwordValue << L" DWORD data: " << dwordData << std::endl;
+
+        ULONGLONG qwordData = RegGetQword(HKEY_CURRENT_USER, testSubkey, testQwordValue);
+        std::wcout << testSubkey << L"\\" << testQwordValue << L" QWORD data: " << qwordData << std::endl;
+
+        std::wstring stringData = RegGetString(HKEY_CURRENT_USER, testSubkey, testStringValue);
+        std::wcout << testSubkey << L"\\" << testStringValue << L" string data: " << stringData << std::endl;
     }
     catch (const RegistryError& e) {
         std::cerr << "error: " << e.ErrorCode() << std::endl;
