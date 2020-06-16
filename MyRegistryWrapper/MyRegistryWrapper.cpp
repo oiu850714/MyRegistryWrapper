22f72c396447d50797dd1b5859a7fdafd32e8e22
oiu850714 <oiu850714@gmail.com>
2020-06-16 14:41:26 +0800

    add simple get DWORD registry value example

--- MyRegistryWrapper/MyRegistryWrapper.cpp
+++ MyRegistryWrapper/MyRegistryWrapper.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include "RegistryWrapper.h"
 
 #include <string>
 #include <iostream>
@@ -7,4 +8,13 @@ int main()
 {
     std::wstring testSubkey = L"Name\\Your\\Subkey";
     std::wstring testValue = L"NameYourValue";
+
+    try
+    {
+        DWORD data = RegGetDword(HKEY_CURRENT_USER, testSubkey, testValue);
+        std::wcout << testSubkey << L"\\" << testValue << L" DWORD data: " << data << std::endl;
+    }
+    catch (const RegistryError& e) {
+        std::cerr << "error: " << e.ErrorCode() << std::endl;
+    }
 }
