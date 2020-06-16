dc09cab2558cb92c5e04a21daf7b764b26435f3d
oiu850714 <oiu850714@gmail.com>
2020-06-16 14:35:54 +0800

    add project files

---
+++ MyRegistryWrapper/MyRegistryWrapper.cpp
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+#include <string>
+#include <iostream>
+
+int main()
+{
+    std::wstring testSubkey = L"Name\\Your\\Subkey";
+    std::wstring testValue = L"NameYourValue";
+}
